6e8aea755be8f73ae72e6f05a1dd2c783051d97d
Gustavo Moisés Tartas <gustavo.tartas@compasso.com.br>
2019-05-28 14:32:00 -0300

    decidido fazer via html

--- js/validaçãoDosCampos/validaNome.js
+++
@@ -1,8 +0,0 @@
-function validaNome(id){ 
-    var nome = document.getElementById(id).value;
-    var padrao = /[^a-zà-ú]/gi;
-    var valida_nome = nome.match(padrao);
-    if( valida_nome || !nome || nome.lenght < 3 ){
-       alert("Nome não foi preenchido, possui caracteres inválidos, ou é muito pequeno!")
-    }
- }
--- js/validaçãoDosCampos/validaOption.js
+++
@@ -1,8 +0,0 @@
-function valida(id){
-    var campo = document.getElementById(id).value;
-       if (id.sel.value=="")
-         {
-            alert("Escolha uma opção!");
-            id.sel.focus();
-         }
-   }
