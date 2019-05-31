33f91e244c3f896ba12cf6ebea11a16b46ca54eb
Bruno Testolin <bruno.testolin@compasso.com.br>
2019-05-31 09:57:06 -0300

    Reciclando pastas

--- Uri/testando.php
+++ Uri/testando.php
@@ -1,5 +1,5 @@
+   
 <?php
-
 require_once('../anexos/conexao.php');
 
 $file_tmp = $_FILES["anexo"]["tmp_name"];
@@ -13,19 +13,20 @@ if($arquivo = isset($_FILES["anexo"])) {
     FALSE;
 }
 
+//print_r($file_tmp);
+
 if(!$arquivo) {
     echo "Houve um erro no Upload!";
 } else {     
     copy($file_tmp, "C:/xampp/htdocs/$file_name");  
+    //move_uploaded_file($file_tmp,"C:/xampp/htdocs/$file_name");
     
     $binario = file_get_contents($file_tmp);
     $binario = mysqli_real_escape_string($conn, $binario);
     
     $sql = "INSERT INTO `binario`.`arquivos` (`Codigo` , `Descricao` , `Arquivo` ,`Tipo` ,`Tamanho` ,`DtHrEnvio`)
     VALUES ('NULL', '$file_name', '$binario', '$file_type', '$file_size', CURRENT_TIMESTAMP)";
-
     mysqli_query($conn,"$sql") or die (mysqli_error($conn));
     echo "Upload completo com sucesso!";
 }
-
 ?>
