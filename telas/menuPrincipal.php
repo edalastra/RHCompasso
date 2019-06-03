16a4fd3ecaf41caa8ff952b6027c709e2abb6a64
Gustavo Moisés Tartas <gustavo.tartas@compasso.com.br>
2019-06-03 13:25:35 -0300

    tudo feito, duas funções mask

--- telas/menuPrincipal.php
+++ telas/menuPrincipal.php
@@ -563,7 +563,7 @@ if (isset($_POST['botaoVolta'])) {
                             <td><select id="add-tipo" name='tipo' class="selectadd intable" required><option value="" selected="selected"></option><option value="1">CLT</option><option value="2">CC</option><option value="3">HO</option><option value="4">TEMP</option><option value="5">APDZ</option></select></td>
                             <td><select id="add-captacao" name='captacao' class="selectadd intable" required><option value="" selected="selected"></option><option value="1">Ex-Funcionario</option><option value="2">Ex-Bolsista</option><option value="3">Ex-Estagiario</option><option value="4">Novo</option></select></td>
                             <td id='add-carga_horaria'><input class='intable' type="number" min="1" max="60" name="carga_horaria" required></td>
-                            <td id='add-horario'><input class='intable' type="text" name="horario" required maxlength="13" onkeypress="valida_horas(this)"></td>
+                            <td id='add-horario'><input class='intable' type="text" name="horario" required maxlength="13" pattern="[0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}[\s]-[\s][0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}" onkeypress="valida_horas(this)"></td>
                             <td id='add-nome'><input class='intable' type="text" name="nome" required></td>
                             <td><select name="sexo" class="intable" value="<?=$rows_dados['SEXO']?>" required>
                                 <option value="" selected="selected"></option>
@@ -572,7 +572,7 @@ if (isset($_POST['botaoVolta'])) {
                                 <option>Feminino</option>
                                 <option>Não definido</option>
                             </select></td>
-                            <td id='add-fone'><input class='intable' type="text" name="fone_contato" id="telefone" maxlength="15" required></td>
+                            <td id='add-fone'><input class='intable' type="text" pattern="\([0-9]{2}\)[\s][0-9]{1}[0-9]{4}-[0-9]{4}" name="fone_contato" id="telefone" maxlength="15" required></td>
                             <td id='add-cargo'><input class='intable' type="text" name="cargo" required></td>
                             <td id='add-contole-data'><input class='intable' type="date" name="controle_data_admissao" required></td>
                             <td id='add-remuneracao'><input class='intable' type="number" step=".01" name="remuneracao_base" required></td>
