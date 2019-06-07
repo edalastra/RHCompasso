1462b37e167a4720e3fc0a165586ad5e7f85c475
Francisco Zorzi <francisco.zorzi_BOLS@pampa.compasso>
2019-06-07 09:32:58 -0300

    Campo Atualizado

--- js/validaCargo.js
+++ js/validaCargo.js
@@ -2,7 +2,7 @@ function validaCargo(){
     var selecionaCargo = $("#add-tipo");
     var campoHoras = $("#campo-carga_horaria");
 
-    if(selecionaCargo.val() == '2' || selecionaCargo.val() == 3){
+    if(selecionaCargo.val() == 2 || selecionaCargo.val() == 3){
         campoHoras.attr("disabled",true);
     }else{
         campoHoras.attr("disabled",false);
